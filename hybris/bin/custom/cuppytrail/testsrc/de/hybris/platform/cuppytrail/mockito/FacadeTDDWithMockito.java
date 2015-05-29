eb352df5632944a1126555fa8786176365b7b2b7
YongJun Chee <yongjun.chee@arvatosystems.com>
2015-05-29 17:17:59 -0400

    Testing the Facades with Mockito - Test 1

---
+++ hybris/bin/custom/cuppytrail/testsrc/de/hybris/platform/cuppytrail/mockito/FacadeTDDWithMockito.java
@@ -0,0 +1,28 @@
+package de.hybris.platform.cuppytrail.mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import de.hybris.platform.cuppytrail.web.data.StadiumDataTO;
+import de.hybris.platform.cuppytrail.web.facades.StadiumFacade;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FacadeTDDWithMockito
+{
+	private StadiumFacade stadiumFacade;
+
+
+	@Test
+	public void testFacade1()
+	{
+		final List<StadiumDataTO> stadiumDataTOs = stadiumFacade.getAllStadium();
+		assertEquals(1, stadiumDataTOs.size());
+		final StadiumDataTO stadiumDataTO = stadiumFacade.getStadiumDetails("Wembley");
+		assertNotNull(stadiumDataTO);
+	}
+}
