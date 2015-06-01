aa6336c57fe87230c1e44d0608f0eed3e4c9ebda
YongJun Chee <yongjun.chee@arvatosystems.com>
2015-06-01 16:45:08 -0400

    Events - Created a custom event

---
+++ hybris/bin/custom/cuppytrail/src/de/hybris/platform/cuppytrail/events/CapacityEvent.java
@@ -0,0 +1,34 @@
+package de.hybris.platform.cuppytrail.events;
+ 
+import de.hybris.platform.servicelayer.event.events.AbstractEvent;
+ 
+ 
+public class CapacityEvent extends AbstractEvent
+{
+    private final Integer capacity;
+    private final String code;
+ 
+    public CapacityEvent(final String code, final Integer capacity)
+    {
+        super();
+        this.code = code;
+        this.capacity = capacity;
+    }
+ 
+    public Integer getCapacity()
+    {
+        return capacity;
+    }
+ 
+    public String getCode()
+    {
+        return code;
+    }
+ 
+    @Override
+    public String toString()
+    {
+ 
+        return this.code + "(" + this.capacity + ")";
+    }
+}
