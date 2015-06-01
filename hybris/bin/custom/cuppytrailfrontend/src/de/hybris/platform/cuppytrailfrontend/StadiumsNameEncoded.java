469e65cc65ff117923655a7fb2cd8c0b0de84a39
YongJun Chee <yongjun.chee@arvatosystems.com>
2015-06-01 12:13:36 -0400

    Front end - Created a Helper class (name encoding)

---
+++ hybris/bin/custom/cuppytrailfrontend/src/de/hybris/platform/cuppytrailfrontend/StadiumsNameEncoded.java
@@ -0,0 +1,24 @@
+package de.hybris.platform.cuppytrailfrontend;
+ 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+ 
+import org.apache.log4j.Logger;
+ 
+public class StadiumsNameEncoded
+{
+    private final static Logger LOG = Logger.getLogger(StadiumsNameEncoded.class);
+ 
+    public static String getNameEncoded(final String name)
+    {
+        try
+        {
+            return URLEncoder.encode(name, "UTF-8");
+        }
+        catch (final UnsupportedEncodingException e)
+        {
+            LOG.error("Problems while encoding", e);
+            return "";
+        }
+    }
+}
